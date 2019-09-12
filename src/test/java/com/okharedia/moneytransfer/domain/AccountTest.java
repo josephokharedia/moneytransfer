bc792a223e5ffcde935772159367c4689cf14323
Joseph Okharedia <joseph.okharedia@spark.net>
2019-09-12 17:27:04 +0200

    Add tests
    - Test account entity
    - Test transfer money service

---
+++ src/test/java/com/okharedia/moneytransfer/domain/AccountTest.java
@@ -0,0 +1,58 @@
+package com.okharedia.moneytransfer.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    public void testDeposit() {
+        Account account = new Account(UUID.randomUUID().toString());
+        account.setBalance(BigDecimal.valueOf(100));
+
+        account.deposit(BigDecimal.valueOf(100));
+        assert account.getBalance().equals(BigDecimal.valueOf(200));
+    }
+
+    @Test
+    public void throwException_whenDepositNegativeAmount() {
+        Account account = new Account(UUID.randomUUID().toString());
+        account.setBalance(BigDecimal.valueOf(100));
+
+        assertThrows(IllegalArgumentException.class, () ->
+                account.deposit(BigDecimal.valueOf(-100)));
+    }
+
+    @Test
+    public void testWithdrawal() throws InsufficientFundsException {
+        Account account = new Account(UUID.randomUUID().toString());
+        account.setBalance(BigDecimal.valueOf(100));
+
+        account.withdraw(BigDecimal.valueOf(50));
+        assert account.getBalance().equals(BigDecimal.valueOf(50));
+    }
+
+    @Test
+    public void throwException_whenWithdrawNegativeAmount() {
+        Account account = new Account(UUID.randomUUID().toString());
+        account.setBalance(BigDecimal.valueOf(100));
+
+        assertThrows(IllegalArgumentException.class, () ->
+                account.withdraw(BigDecimal.valueOf(-100)));
+    }
+
+    @Test
+    public void throwException_whenWithdrawalInsufficientFunds() {
+        Account account = new Account(UUID.randomUUID().toString());
+        account.setBalance(BigDecimal.valueOf(100));
+
+        assertThrows(InsufficientFundsException.class, () ->
+                account.withdraw(BigDecimal.valueOf(200)));
+    }
+
+
+}
---
+++ src/test/java/com/okharedia/moneytransfer/service/DefaultMoneyTransferServiceTest.java
@@ -0,0 +1,61 @@
+package com.okharedia.moneytransfer.service;
+
+import com.okharedia.moneytransfer.domain.Account;
+import com.okharedia.moneytransfer.domain.AccountNotFoundException;
+import com.okharedia.moneytransfer.domain.InsufficientFundsException;
+import com.okharedia.moneytransfer.domain.internal.DefaultMoneyTransferService;
+import com.okharedia.moneytransfer.repository.DefaultAccountRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+class DefaultMoneyTransferServiceTest {
+
+    @Mock
+    private DefaultAccountRepository accountRepository;
+    @InjectMocks
+    private DefaultMoneyTransferService moneyTransferService;
+    private Account fromAccount;
+    private Account toAccount;
+
+    @BeforeEach
+    private void setup() {
+        MockitoAnnotations.initMocks(this);
+        fromAccount = new Account("FROM");
+        fromAccount.setBalance(BigDecimal.valueOf(100));
+
+        toAccount = new Account("TO");
+        toAccount.setBalance(BigDecimal.valueOf(0));
+
+        when(accountRepository.getAccountByAccountNumber(fromAccount.getAccountNumber()))
+                .thenReturn(Optional.of(fromAccount));
+
+        when(accountRepository.getAccountByAccountNumber(toAccount.getAccountNumber()))
+                .thenReturn(Optional.of(toAccount));
+    }
+
+    @Test
+    public void testMoneyTransfer() throws InsufficientFundsException, AccountNotFoundException {
+
+        moneyTransferService.transferMoney(
+                fromAccount.getAccountNumber(),
+                toAccount.getAccountNumber(),
+                BigDecimal.ONE);
+
+        assert fromAccount.getBalance().equals(BigDecimal.valueOf(99));
+        assert toAccount.getBalance().equals(BigDecimal.valueOf(1));
+
+        verify(accountRepository).getAccountByAccountNumber(fromAccount.getAccountNumber());
+        verify(accountRepository).getAccountByAccountNumber(toAccount.getAccountNumber());
+
+        verify(accountRepository).updateBalance(fromAccount.getAccountNumber(), fromAccount.getBalance());
+        verify(accountRepository).updateBalance(toAccount.getAccountNumber(), fromAccount.getBalance());
+    }
+}
