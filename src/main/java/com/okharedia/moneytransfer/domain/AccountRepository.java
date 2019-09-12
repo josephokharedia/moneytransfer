0afe2a9676f314df31c41b378804270bf4c8ad37
Joseph Okharedia <joseph.okharedia@spark.net>
2019-09-12 22:27:52 +0200

    Add Documentation
    - Document account repository interface
    - Document money transfer interface

--- src/main/java/com/okharedia/moneytransfer/domain/AccountRepository.java
+++ src/main/java/com/okharedia/moneytransfer/domain/AccountRepository.java
@@ -5,7 +5,20 @@ import java.util.Optional;
 
 public interface AccountRepository {
 
+    /**
+     * Update current balance on an account
+     *
+     * @param accountNumber - accountNumber of account
+     * @param newBalance    - new current balance of the account
+     * @throws AccountNotFoundException - when account is not found
+     */
     void updateBalance(String accountNumber, BigDecimal newBalance) throws AccountNotFoundException;
 
+    /**
+     * Find account by account number
+     *
+     * @param accountNumber - accountNumber of account
+     * @return - account
+     */
     Optional<Account> getAccountByAccountNumber(String accountNumber);
 }
--- src/main/java/com/okharedia/moneytransfer/domain/MoneyTransferService.java
+++ src/main/java/com/okharedia/moneytransfer/domain/MoneyTransferService.java
@@ -4,6 +4,15 @@ import java.math.BigDecimal;
 
 public interface MoneyTransferService {
 
+    /**
+     * Transfer amount between accounts
+     *
+     * @param fromAccountNumber - fromAccount number to be debited
+     * @param toAccountNumber   - toAccount number to be credited
+     * @param amount            - amount to be transferred
+     * @throws InsufficientFundsException - when fromAccount balance is lower than amount
+     * @throws AccountNotFoundException   - when either fromAccount or toAccount is not found
+     */
     void transferMoney(String fromAccountNumber, String toAccountNumber, BigDecimal amount)
             throws InsufficientFundsException, AccountNotFoundException;
 }
